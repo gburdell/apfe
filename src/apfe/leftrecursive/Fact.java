cf97b7303be4414b1cbcbcf79876ae488905ad37
George P Burdell <gburdell77@gmail.com>
2014-10-15 09:51:11 -0700

    started testing of direct left recursion

--- src/apfe/leftrecursive/Fact.java
+++ src/apfe/leftrecursive/Fact.java
@@ -1,34 +1,14 @@
-/*
- * The MIT License
- *
- * Copyright 2013 gburdell.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-package apfe.leftrecursive;
 
-import apfe.runtime.LeftRecursiveAcceptor;
-import apfe.runtime.Acceptor;
-import apfe.runtime.CharClass;
-import apfe.runtime.Sequence;
+package apfe.leftrecursive ;
 
-public class Fact extends LeftRecursiveAcceptor {
+
+import apfe.runtime.*;
+//import apfe.leftrecursive.*;
+
+
+
+
+public  class Fact extends LeftRecursiveAcceptor {
 
     public Fact() {
     }
@@ -49,54 +29,39 @@ public class Fact extends LeftRecursiveAcceptor {
 
     @Override
     public Acceptor getChoice(int ix) {
-        Acceptor a = null;
-        switch (ix) {
-            case 0:
-                a = new Sequence(this,//new Fact(),
-                        new CharClass("*"),
-                        new Fact(true));//for definite right recursion
-                break;
-            case 1:
-                a = new Sequence(this,//new Fact(),
-                        new CharClass("/"),
-                        new Fact(true));//for definite right recursion
-                break;
-            case 2:
-                a = new Prim();
-                break;
-        }
-        //Keep track so if we growSeed()
-        m_lastIx = ix;
-        m_lastChoice = a;
-        return a;
-    }
+        Acceptor matcher = null  ;
+		switch(ix) {
+		case 0:
+matcher = new Sequence(this,
+new CharSeq('*'),
+new Fact()) ;
+break;
+case 1:
+matcher = new Sequence(this,
+new CharSeq('/'),
+new Fact(true)) ;
+break;
+case 2:
+matcher = new Prim() ;
+break;
 
-    //@Override
-    public Acceptor getChoiceXX(int ix) {
-        Acceptor a = null;
-        switch (ix) {
-            case 0:
-                a = new Sequence(this,//new Fact(),
-                        new CharClass("*"),
-                        new Prim());
-                break;
-            case 1:
-                a = new Sequence(this,//new Fact(),
-                        new CharClass("/"),
-                        new Prim());
-                break;
-            case 2:
-                a = new Prim();
-                break;
-        }
+		}
         //Keep track so if we growSeed()
         m_lastIx = ix;
-        m_lastChoice = a;
-        return a;
+        m_lastChoice = matcher;
+        return matcher;
     }
 
     @Override
-    public String getNonTermID() {
-        return getClass().getSimpleName();
+    protected boolean accepti() {
+		boolean match = super.accepti();
+
+        return match;
     }
+
+ 
+
+
 }
+
+
--- src/apfe/leftrecursive/Prim.java
+++ src/apfe/leftrecursive/Prim.java
@@ -1,76 +1,51 @@
-/*
- * The MIT License
- *
- * Copyright 2013 gburdell.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-package apfe.leftrecursive;
 
-import apfe.runtime.Acceptor;
-import apfe.runtime.CharSeq;
-import apfe.runtime.PrioritizedChoice;
-import apfe.runtime.Sequence;
+package apfe.leftrecursive ;
 
-public class Prim extends Acceptor {
+
+import apfe.runtime.*;
+//import apfe.leftrecursive.*;
+
+
+
+
+public  class Prim extends Acceptor {
 
     public Prim() {
     }
 
     @Override
     protected boolean accepti() {
-        PrioritizedChoice pc1 = new PrioritizedChoice(new PrioritizedChoice.Choices() {
-            @Override
-            public Acceptor getChoice(int ix) {
-                Acceptor a = null;
-                switch (ix) {
-                    case 0:
-                        a = new Sequence(new CharSeq('('), new Term(), new CharSeq(')'));
-                        break;
-                    /**/
-                    case 1:
-                        a = new LrInteger();
-                        break;
-                }
-                return a;
-            }
-        });
-        boolean match = (null != (pc1 = match(pc1)));
-        if (match) {
-            m_val = pc1.getAccepted();
-        }
+		Acceptor matcher = new PrioritizedChoice(new PrioritizedChoice.Choices() {
+@Override
+public Acceptor getChoice(int ix) {
+Acceptor acc = null;
+switch (ix) {
+case 0:
+acc = new Sequence(new CharSeq('('),
+new Term(),
+new CharSeq(')')) ;
+break;
+case 1:
+acc = new Int() ;
+break;
+}
+return acc;
+}
+}) ;
+		Acceptor accepted = match(matcher);
+		boolean match = (null != accepted);
+
         return match;
     }
-    private Acceptor m_val;
 
     @Override
-    public Acceptor create() {
+    public Prim create() {
         return new Prim();
     }
 
-    @Override
-    public String toString() {
-        return m_val.toString();
-    }
+ 
 
-    @Override
-    public String getNonTermID() {
-        return getClass().getSimpleName();
-    }
+
 }
+
+
