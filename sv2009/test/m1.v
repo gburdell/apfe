e1a7e5acda63bb3f71207032e58ab9efbdf41c56
George P. Burdell <gburdell77@gmail.com>
2014-10-22 08:18:03 -0700

    bug in m1.v

--- sv2009/test/m1.v
+++ sv2009/test/m1.v
@@ -1,5 +1,12 @@
 module foo();
-output wire ww;
+output wire ww, ww2;
 integer fives[4] = '{ 5, 10, 15, 20 };
 integer threes[4] = ' { 3, 6, 9, 12 };
+endmodule
+
+module bar(
+	input wire clk,
+	output wire z,
+	output reg x
+);
 endmodule
