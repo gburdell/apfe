f5f410008f851dc49332150e50753d8685deb6a9
George P. Burdell <gburdell77@gmail.com>
2014-10-22 19:06:17 -0700

    to_cloud

--- sv2009/test/m1.v
+++ sv2009/test/m1.v
@@ -1,12 +1,18 @@
+module bar(
+input wire clk,
+output wire z,
+output reg x
+);
+function [49:0] fn1;
+input [13:0] x;         
+input [13:0] y;         
+input z;
+endfunction
+
+endmodule
 module foo();
 output wire ww, ww2;
 integer fives[4] = '{ 5, 10, 15, 20 };
 integer threes[4] = ' { 3, 6, 9, 12 };
 endmodule
 
-module bar(
-	input wire clk,
-	output wire z,
-	output reg x
-);
-endmodule
